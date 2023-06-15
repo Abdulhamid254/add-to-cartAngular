18db3fd1d5f4d0280d365601f5758a66fb72a499
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-15 01:27:16 -0700

    Added the cart services & created the relevant methods,aslo implemented the beahavioursubject

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  public cartItemList: any = [];
+  // productlist below will both work as observable and also responsible for emmiting the data
+  public productList = new BehaviorSubject<any>([]); // here we have initialsed as empty array as our initial value because our product list is an array
+
+  constructor() {}
+  // whoever will make use of the getProducts method he will subscribe to it
+  getProducts() {
+    return this.productList.asObservable();
+  }
+  // in the setter we are pushing the product into the cartList then we are emmitting the data next used for emitting
+  setProduct(product: any) {
+    this.cartItemList.push(...product);
+    this.productList.next(product);
+
+    // next here helps in passing data whererever the productlist is subscribed
+  }
+  addtoCart(product: any) {
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+  }
+  getTotalPrice() {
+    let grandTotal = 0;
+    this.cartItemList.map((a: any) => {
+      //a here is everything inside the cartItemlist
+      grandTotal += a.total;
+    });
+  }
+  removeCartItem(product: any) {
+    //removing 1 item from our array
+    this.cartItemList.map((a: any, index: any) => {
+      if (product.id === a.id) {
+        this.cartItemList.splice(index, 1);
+      }
+    });
+  }
+  removeAllCart() {
+    this, (this.cartItemList = []);
+    this.productList.next(this.cartItemList); //resetting after subscribing
+  }
+}
